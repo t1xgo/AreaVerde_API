85280235500c9de939965a97dda87eeef678403e
Alejafranco <mafa1008@gmail.com>
2021-11-06 22:34:25 -0500

    Listar reportes admin y user

--- app/controllers/reportes/reportes.js
+++ app/controllers/reportes/reportes.js
@@ -4,53 +4,42 @@ const { createFolder, saveFile, readDirectory } = require("../../services/fs.ser
 
 //Crear reporte
 const createReport = async (req, res) => {
-    let report = req.body;
-    let sql = `insert into reportes (descripcion, id_categoria, id_usuario, ubicacion, rutaimagen, 
+  let report = req.body;
+  let sql = `insert into reportes (descripcion, id_categoria, id_usuario, ubicacion, rutaimagen, 
             estado) values('${report.descripcion}', '${report.id_categoria}', 
             ${report.id_usuario}, '${report.ubicacion}', 
             './docs/${report.id_usuario}/${report.rutaimagen}', '${report.estado}')`;
-    try {
-        let result = await _pg.executeSql(sql);
-        return res.send({
-            ok: result.rowCount == 1,
-            message: result == 1 ? "El reporte no fue creado" : "Reporte creado",
-            content: {report,id:report.id_usuario},
-        });
-    } catch (error) {
-        return res.send({
-            ok: false,
-            message: "Error creando el reporte",
-            content: error,
-        });
-    }
+  try {
+    let result = await _pg.executeSql(sql);
+    return res.send({
+      ok: result.rowCount == 1,
+      message: result == 1 ? "El reporte no fue creado" : "Reporte creado",
+      content: { report, id: report.id_usuario },
+    });
+  } catch (error) {
+    return res.send({
+      ok: false,
+      message: "Error creando el reporte",
+      content: error,
+    });
+  }
 };
+
 //Traer TODOS los reportes
 const getReportes = async (req, res) => {
   try {
     let user = req.body;
-    if(user.rol == 0 || user.rol == undefined)
-    {
-      let sql = `select id_reporte, rutaimagen, descripcion, ubicacion, estado from reportes`;
-      let result = await _pg.executeSql(sql);
-      let rows = result.rows;
-      return res.send({
-        ok: true,
-        message: "Reportes consultados",
-        content: rows,
-      });
-    }
-    else
-    {
-      let sql = `select id_reporte, rutaimagen, descripcion, ubicacion,
-       estado from reportes where estado = 1 or estado = 2`;
-      let result = await _pg.executeSql(sql);
-      let rows = result.rows;
-      return res.send({
-        ok: true,
-        message: "Reportes consultados",
-        content: rows,
-      });
-    }
+    let sql = `select id_reporte, rutaimagen, reportes.descripcion as descripcion, ubicacion, estado, categorias.nombre as categoria
+      from reportes inner join
+      categorias on categorias.id_categoria = reportes.id_categoria where estado = 0`;
+    let result = await _pg.executeSql(sql);
+    let rows = result.rows;
+    return res.send({
+      ok: true,
+      message: "Reportes consultados",
+      content: rows,
+    });
+
   } catch (error) {
     return res.send({
       ok: false,
@@ -62,55 +51,55 @@ const getReportes = async (req, res) => {
 
 // Traer los reportes de UN USUARIO
 const getReport = async (req, res) => {
-    try {
-      let id = req.params.id;
-      let sql = `select id_reporte, rutaimagen, descripcion, ubicacion, 
+  try {
+    let id = req.params.id;
+    let sql = `select id_reporte, rutaimagen, descripcion, ubicacion, 
       estado from reportes where id_usuario = ${id}`;
-      let result = await _pg.executeSql(sql);
-      let rows = result.rows;
-      rows = rows.map((report) =>{
-        let path = `./docs/${report.id_usuario}`;
-        report.rutaimagen = readDirectory(path);
-        return report;
-      });
-      return res.send({
-        ok: true,
-        message: "Reportes consultados del usuario",
-        content: rows,
-      });
-    } catch (error) {
-      return res.send({
-        ok: false,
-        message: "Ha ocurrido un error consultando los reportes del usuario",
-        content: error,
-      });
-    }
+    let result = await _pg.executeSql(sql);
+    let rows = result.rows;
+    rows = rows.map((report) => {
+      let path = `./docs/${id}`;
+      report.rutaimagen = readDirectory(path);
+      return report;
+    });
+    return res.send({
+      ok: true,
+      message: "Reportes consultados del usuario",
+      content: rows,
+    });
+  } catch (error) {
+    return res.send({
+      ok: false,
+      message: "Ha ocurrido un error consultando los reportes del usuario",
+      content: error,
+    });
+  }
 };
 //Guardar imagenes de reportes
 const saveFiles = async (req, res) => {
-    try {
-      console.log('SAVEFLES()');
-      let id = req.params.id;
-      let files = req.files;
-      let image = files.imagen;
-      let pathReportes = `./docs/${id}/`;
-      console.log(pathReportes);
-      createFolder(pathReportes);
-      saveFile(`${pathReportes}${image.name}`, image.data);
-      console.log('SAVEFLES()');
-      return res.send({
-        ok: true,
-        message: "Archivos cargados.",
-        content: {},
-      });
-    } catch (error) {
-      console.log('ERROR SAVEFILES', error);
-      return res.status(500).send({
-        ok: false,
-        message: "Ha ocurrido un error subiendo el archivo",
-        content: error,
-      });
-    }
-  };
+  try {
+    console.log('SAVEFLES()');
+    let id = req.params.id;
+    let files = req.files;
+    let image = files.imagen;
+    let pathReportes = `./docs/${id}/`;
+    console.log(pathReportes);
+    createFolder(pathReportes);
+    saveFile(`${pathReportes}${image.name}`, image.data);
+    console.log('SAVEFLES()');
+    return res.send({
+      ok: true,
+      message: "Archivos cargados.",
+      content: {},
+    });
+  } catch (error) {
+    console.log('ERROR SAVEFILES', error);
+    return res.status(500).send({
+      ok: false,
+      message: "Ha ocurrido un error subiendo el archivo",
+      content: error,
+    });
+  }
+};
 
-module.exports = { createReport,getReportes, getReport, saveFiles };
+module.exports = { createReport, getReportes, getReport, saveFiles };
